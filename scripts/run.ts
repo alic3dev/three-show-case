8b35e3240b66335dc0b2ce32fbdbdc5dae156c81
alic3 <alic3dev@gmail.com>
2024-06-20 03:52:11 -0400

    Add script runner

---
+++ scripts/run.ts
@@ -0,0 +1,40 @@
+import path from 'path'
+
+import 'module-alias/register'
+import { addAliases } from 'module-alias'
+
+addAliases({
+  '@': `${__dirname}/../src/`,
+})
+
+import fs from 'fs'
+
+const scriptName: string | undefined = process.argv[2]
+
+if (!scriptName) {
+  console.error('Pass the name of the script as the next argument.')
+  process.exit(1)
+}
+
+let scriptPath: string = path.join(__dirname, scriptName)
+
+if (!fs.existsSync(scriptPath)) {
+  const possibleExtensions: string[] = ['.ts', '.tsx', '.js', '.jsx']
+
+  for (const ext of possibleExtensions) {
+    scriptPath = path.format({
+      dir: __dirname,
+      name: scriptName,
+      ext,
+    })
+
+    if (fs.existsSync(scriptPath)) break
+  }
+
+  if (!fs.existsSync(scriptPath)) {
+    console.error(`Couldn't find script: ${process.argv[2]}`)
+    process.exit(1)
+  }
+}
+
+require(scriptPath)
