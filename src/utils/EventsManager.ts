ca350e07cd889c544574b3bf14f7e3864a49e9ee
alic3 <alic3dev@gmail.com>
2024-07-19 10:56:12 -0400

    Add container events to `EventsManager`

--- src/utils/EventsManager.ts
+++ src/utils/EventsManager.ts
@@ -1,3 +1,8 @@
+interface ContainerEvent {
+  type: keyof HTMLElementEventMap
+  listener: EventListener
+}
+
 interface DocumentEvent {
   type: keyof DocumentEventMap
   listener: EventListener
@@ -9,8 +14,29 @@ interface WindowEvent {
 }
 
 export class EventsManager {
+  private containerEvents: ContainerEvent[] = []
   private documentEvents: DocumentEvent[] = []
   private windowEvents: WindowEvent[] = []
+
+  private containerElement?: HTMLElement
+
+  constructor(containerElement?: HTMLElement) {
+    this.containerElement = containerElement
+  }
+
+  addContainerEvent<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (event: HTMLElementEventMap[K]) => void,
+  ): void {
+    if (!this.containerElement) throw new Error('No container element')
+
+    this.containerElement.addEventListener(type, listener)
+
+    this.containerEvents.push({
+      type,
+      listener: listener as EventListener,
+    })
+  }
 
   addDocumentEvent<K extends keyof DocumentEventMap>(
     type: K,
@@ -36,6 +62,16 @@ export class EventsManager {
     })
   }
 
+  removeContainerEvents(): void {
+    if (!this.containerElement) return
+
+    for (const event of this.containerEvents) {
+      this.containerElement.removeEventListener(event.type, event.listener)
+    }
+
+    this.containerEvents.splice(0)
+  }
+
   removeDocumentEvents(): void {
     for (const event of this.documentEvents) {
       document.removeEventListener(event.type, event.listener)
@@ -53,6 +89,7 @@ export class EventsManager {
   }
 
   removeAllEvents(): void {
+    this.removeContainerEvents()
     this.removeDocumentEvents()
     this.removeWindowEvents()
   }
